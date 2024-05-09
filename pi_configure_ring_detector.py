b9a0dda99112aa177a9dcd96cd8a90495058b963
marzeverett <marzeverett@gmail.com>
2024-05-08 18:28:54 -0700

    Hopefull another performance improvement

--- pi_configure_ring_detector.py
+++ pi_configure_ring_detector.py
@@ -11,7 +11,7 @@ from picamera2 import Picamera2
  
 
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (1920, 1080)
+picam2.preview_configuration.main.size = (960, 540)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.start()
 
